f12fb47f35b1af9b491c02aa0793254e10d84516
Rodrigo Sibada <pintorodrigo597@gmail.com>
2026-09-10 11:41:41 -0500

    Refactor App component to use AppProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import './App.css';
+import { AppProvider } from './contexts/AppContext';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Sidebar from './Components/Sidebar';
@@ -8,8 +9,10 @@ import Cursos from './Components/Cursos';
 import Login from './Components/Login';
 import Registro from './Components/Registro';
 import Configuracion from './Components/Configuracion';
+import ContadorRegistros from './Components/ContadorRegistros';
+import PanelNotificaciones from './Components/PanelNotificaciones';
 
-function App() {
+function AppContent() {
   const [pagina, setPagina] = useState<string>('cursos');
 
   // Las páginas de auth no llevan Header/Sidebar/Footer
@@ -39,6 +42,14 @@ function App() {
         <Sidebar pagina={pagina} setPagina={setPagina} />
 
         <main className="lynko-contenido">
+          {/* Mostrar componentes globales en todas las páginas */}
+          {pagina !== 'login' && pagina !== 'registro' && (
+            <>
+              <PanelNotificaciones />
+              <ContadorRegistros />
+            </>
+          )}
+
           {pagina === 'cursos' && <Cursos />}
           {pagina === 'usuarios' && <Usuarios />}
           {pagina === 'config' && <Configuracion />}
@@ -47,6 +58,14 @@ function App() {
 
       <Footer />
     </div>
+  );
+}
+
+function App() {
+  return (
+    <AppProvider>
+      <AppContent />
+    </AppProvider>
   );
 }
 
